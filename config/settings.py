5bd96af07b65bb5029262a23db05354624c2cc75
yb song <dudqln2005@gmail.com>
2021-07-14 18:02:14 +0900

    로그인,로그아웃 기능구현 완료 및 회원가입 완성했습니다

--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 로그인 성공후 이동하는 URL
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from question import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('question/', include('question.urls')),
+    path('', views.index, name='index'),
     path('common/', include('common.urls')),
+    path('question/', include('question.urls')),
 ]
