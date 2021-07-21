b3b98664e9cf879fddce7429c6ec0ca86a0bfbb6
yb song <dudqln2005@gmail.com>
2021-07-21 17:05:23 +0900

    파일 인코딩 후 cv2를 활용하여 그림그리기 성공하였습니다

--- photo/views/photo_views.py
+++ photo/views/photo_views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 import base64
 import requests, json
+import cv2
+import numpy as np
+import os
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.conf import settings
 
 from ..models import Photo
 from ..forms import PhotoForm
@@ -18,14 +24,23 @@ def index(request):
 @login_required(login_url='common:login')
 def photo_create(request):
     if request.method == "POST":
+        # 로컬 저장 파일
         filepath = request.FILES['file']
+        path = default_storage.save('content/test.jpg', ContentFile(filepath.read()))
+        file = os.path.join(settings.MEDIA_ROOT, path)
+
+        # 서버에 보내는 파일
         image = request.FILES['file'] #파일 이름 ex)한상.jpg
         encoding_img = base64.b64encode(image.read())
         encoding_img = encoding_img.decode('utf8')
         datas = {'image': encoding_img}
+        datas = json.dumps(datas)
+
+        # 서버 주소
         base_url = "http://3af2ecc4eaa8.ngrok.io"
         url = base_url + '/convert'
-        datas = json.dumps(datas)
+
+        # 서버로 보내기
         res = "test"
         # res = web_request(method_name='POST', url=url, dict_data=datas) #결과값
         # del res['status_code']
@@ -33,9 +48,16 @@ def photo_create(request):
         # del res['Content-Type']
         # del res['ok']
         detection_len = len(res) #데이터 개수
+        result = {'0': [275, 127, 358, 212, 'umook', 1.0], '1': [264, 46, 320, 110, 'gaeranmalee', 0.574],
+                  '2': [179, 44, 246, 107, 'kongnamul', 0.99], '3': [78, 223, 174, 319, 'rice', 1.0],
+                  '4': [102, 52, 162, 102, 'kimchigeon', 1.0], '5': [113, 116, 228, 213, 'kimchigeon', 1.0],
+                  '6': [356, 38, 446, 136, 'kkatip', 0.839], '7': [193, 211, 299, 329, 'mookoook', 0.48]}
+        src = cv2.imread(file, cv2.IMREAD_COLOR)
+        draw =drawing(result, src, file)
+
     else:
         pass
-    context = {'image':image , 'res': res, 'detection_len':detection_len, 'filepath':filepath}
+    context = {'image':image , 'res': res, 'detection_len':detection_len, 'draw': draw, 'filepath': file}
     return render(request, 'photo/photo_result.html', context)
 
 def web_request(method_name, url, dict_data, is_urlencoded=True):
@@ -56,3 +78,16 @@ def web_request(method_name, url, dict_data, is_urlencoded=True):
         return {**dict_meta, **response.json()}
     else:  # 문자열 형태인 경우
         return {**dict_meta, **{'text': response.text}}
+
+
+
+def drawing(result,src, file):
+    color = (0,255,0)
+    tmp = src.copy()
+    # print(type(result))
+    for key,value in result.items():
+        # print(key,value)
+        left, top, right, bottom = value[0],value[1],value[2],value[3]
+        cv2.rectangle(tmp, (left, top), (right, bottom), color, 2)
+        cv2.putText(tmp, "{} [{:.2f}]".format(value[4], float(value[5])),(left, top - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,color, 2)
+    return cv2.imwrite(file, tmp)
